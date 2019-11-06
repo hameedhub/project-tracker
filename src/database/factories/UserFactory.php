8b56e60983fe522e269391dd8ec7bcd09eacce34
Hameed Abdulrahaman <hameedabdulrahamann@gmail.comgit config --global user.name Hameed>
2019-11-06 11:34:25 +0100

    reg edit

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -18,7 +18,10 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->first_name,
+        'last_name'=> $faker->last_name,
+        'phone'=> $faker->phone,
+        'address'=> $faker->address,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
